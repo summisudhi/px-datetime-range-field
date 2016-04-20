1a2bb47b7108ccc2650c5f2c897396bc9be0c1d8
Elliot Nahman <elliot.nahman@ge.com>
2016-04-20 14:56:56 -0700

    updated demo; working on tests

--- test/px-datetime-range-field-custom-tests.js
+++ test/px-datetime-range-field-custom-tests.js
@@ -1,10 +1,39 @@
-// This is the wrapper for custom tests, called upon web components ready state
+
+function runBaseTests() {
+  suite('Polymer exists', function() {
+
+    test('Polymer exists', function() {
+      assert.isTrue(Polymer !== null);
+    });
+  });
+}
+
 function runCustomTests() {
-  // Place any setup steps like variable declaration and initialization here
+  var basic = document.getElementById('basic'),
+      range = document.getElementById('range'),
+      clearInvalid = document.getElementById('clearInvalid'),
+      noClearInvalid = document.getElementById('noClearInvalid'),
+      buttonsDisplayHideSubmit = document.getElementById('buttonsDisplayHideSubmit'),
+      buttonsDisplayHideCancel = document.getElementById('buttonsDisplayHideCancel'),
+      buttonsDisplayHideAll = document.getElementById('buttonsDisplayHideAll'),
+      datetimeButtonsDisplayOptionOnfocus = document.getElementById('datetimeButtonsDisplayOptionOnfocus'),
+      autoSubmit = document.getElementById('autoSubmit');
 
   // This is the placeholder suite to place custom tests in
   // Use testCase(options) for a more convenient setup of the test cases
-  suite('Custom Automation Tests for px-datetime-range-input', function() {
-    
+  suite('Test basic instantiation', function() {
+    checkIfElemExists(basic,'basic');
+
+    test('basic attributes',function(){
+      basic.clearInvalid
+    });
   });
 };
+
+
+
+function checkIfElemExists(elem,str) {
+  test(str + 'fixture is created', function() {
+    assert.isTrue(elem !== null);
+  });
+}
