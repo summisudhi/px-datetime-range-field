7339482d78a08e78291461ae4e22406da822487a
Sirisha Aluri <sirisha.aluri@ge.com>
2016-04-29 12:15:12 -0700

    first push

--- wct.conf.js
+++ wct.conf.js
@@ -31,3 +31,42 @@ module.exports = {
     'test/px-datetime-range-field-test-fixture.html'
   ]
 };
+
+
+module.exports = {
+    verbose: false,
+    plugins: {
+        local: {
+            browsers: ['chrome', 'firefox']
+        },
+        sauce: {
+             "disabled" : false,
+             "browsers": [
+             
+        
+        {
+          "browserName": "chrome",
+          "platform": "Windows 10",
+          "version": "44"
+        },
+         {
+          "browserName": "firefox",
+          "platform": "Windows 8.1",
+          "version": "44"
+        },
+         {
+          "browserName": "safari",
+          "platform": "OS X 10.10",
+          "version": "8"
+        }
+
+        
+        
+      ]
+        }
+    },
+    suites: [
+    'test/px-datetime-range-field-test-fixture.html'
+  ]
+    
+};
